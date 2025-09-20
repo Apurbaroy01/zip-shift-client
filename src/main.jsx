f654a10f641bfb251da1301d5ece8cc24df7996a
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-20 13:34:36 +0600

    7th

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './router/router';
+import AuthProvider from './Context/AuthContext/AuthProvider';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
