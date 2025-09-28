d85c9146c58654620b58aa9d8ef805ad6b0e37b9
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-29 01:10:24 +0600

    21th

--- src/Page/DashBoard/Payment/PaymentFrom.jsx
+++ src/Page/DashBoard/Payment/PaymentFrom.jsx
@@ -1,12 +1,13 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PaymentFrom = () => {
+    const [error, setError] = useState('');
     const stripe = useStripe();
     const elements = useElements();
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (!stripe || !elements) {
             return;
@@ -17,17 +18,48 @@ const PaymentFrom = () => {
         if (!card) {
             return;
         }
+
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if (error) {
+            console.log('error', error);
+            setError(error.message)
+        }
+        else {
+            setError("")
+            console.log('PaymentMethod', paymentMethod);
+        }
     };
 
     return (
-        <div>
+        <div className='w-9/12 mx-auto p-10  justify-center items-center '>
             <form onSubmit={handleSubmit}>
-                <CardElement></CardElement>
+                <div className="border rounded-lg p-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500">
+                    <CardElement
+                        options={{
+                            style: {
+                                base: {
+                                    fontSize: "16px",
+                                    color: "#424770",
+                                    "::placeholder": { color: "#a0aec0" },
+                                },
+                                invalid: { color: "#e53e3e" },
+                            },
+                        }}
+                    />
+                </div>
 
-                <button type='submit' disabled={!stripe}>
+                <button className='btn btn-primary w-full mt-5' type='submit' disabled={!stripe}>
                     Pay
                 </button>
             </form>
+            {
+                error && <p className='text-2xl text-red-600'>{error}</p>
+            }
 
         </div>
     );
