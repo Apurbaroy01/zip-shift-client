8413aca443b45de08f2ba00323c1aa1e75d3438b
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-10-04 00:52:19 +0600

    32th

--- src/Hook/useAxiosSecoure.jsx
+++ src/Hook/useAxiosSecoure.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import useAuth from "./useAuth";
 
 const Axiosinstance = axios.create({
     baseURL: `http://localhost:5000`,
@@ -6,6 +7,13 @@ const Axiosinstance = axios.create({
 
 
 const useAxiosSecoure = () => {
+    const { user } = useAuth();
+    Axiosinstance.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    })
     return Axiosinstance;
 };
 
